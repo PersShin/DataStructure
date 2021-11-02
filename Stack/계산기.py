2943ecc32fa21c7f49ca7ac3853dd54430dd36fc
PersShin <shinryan9@naver.com>
2021-11-02 10:41:11 +0900

    중위계산식에서 후의계산식으로 바꾼 후에 연산을 진행하는 코드

--- Stack/계산기.py
+++ Stack/계산기.py
@@ -1,5 +1,24 @@
-OPERATORS = set(['+', '-', '*', '/', '^'])  # set of operators
-PRIORITY = {'+':1, '-':1, '*':2, '/':2, '^':3}
+OPERATORS = set(['+', '-', '*', '/'])  # set of operators
+PRIORITY = {'+':1, '-':1, '*':2, '/':2}
+def answer(token):
+    stacker=[]
+    for i in token:
+        if(i not in OPERATORS):
+            stacker.append(i)
+        else:
+            try:
+                tmp=int(stacker.pop())
+                temp=int(stacker.pop())
+            except: print('계산식이 잘못 되었습니다!')
+            if(i=='+'):
+                stacker.append(tmp+temp)
+            elif(i=='-'):
+                stacker.append(temp-tmp)
+            elif(i=='*'):
+                stacker.append(temp*tmp)
+            else:
+                stacker.append(temp/tmp)
+    print(stacker.pop())
 def Infix2Postfix(expression):
     stacker=[]
     token=''
@@ -9,16 +28,17 @@ def Infix2Postfix(expression):
             stacker+=i
         elif(i==')'):
             while stacker and stacker[-1]!='(':
-                token+=stacker.pop(-1)
-            stacker.pop(-1)
+                token+=stacker.pop()
+            stacker.pop()
         elif(i not in OPERATORS):
             token+=i
         else:
             while stacker and stacker[-1]!='(' and PRIORITY[i]<=PRIORITY[stacker[-1]]:
-                token+=stacker.pop(-1)
+                token+=stacker.pop()
             stacker.append(i)
     while stacker and stacker[-1]!='(':
-        token+=stacker.pop(-1)
+        token+=stacker.pop()
     print(token)
-expression='(A+(C+D*F+T*A)*B/C+B)'
+    answer(token)
+expression='(1+(2*(3+4)*7))'
 Infix2Postfix(expression)
