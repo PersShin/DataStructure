417927995a73652122e3c3cadeb704b5c2dd313a
PersShin <shinryan9@naver.com>
2021-11-04 19:38:36 +0900

    isNone함수(head가 있는지 없는지 알려주는 함수)와 delete_item함수(지정한 item 삭제하는 함수) 추가

--- List/SingleList.py
+++ List/SingleList.py
@@ -3,54 +3,72 @@ class SingleList:
         def __init__(self,item=None, link=None):
             self._item=item
             self._link=link
-
     def __init__(self):
         self._head=None
         self._size=0
-    def appender_first(self, item):
-        newest=self.Node(item,self._head)
+    def isNone(self):
+        if(self._head is None):
+            return True
+        return False
+    def appender_first(self,item):
+        newest=self.Node(item, self._head)
         self._head=newest
         self._size+=1
-    def appender_last(self,item):
-        if(self._head is None):
-            self.appender_first(item)
+    def appender_last(self, item):
+        if(self.isNone()):
+            self.appender_first(self,item)
         else:
             newest=self.Node(item)
             tail=self._head
-            while tail._link is not None:
+            while(tail._link is not None):
                 tail=tail._link
             tail._link=newest
             self._size+=1
-    def delete_first(self):
-        if(self._size==0):
-            return None
-        self._head=self._head._link
-        self._size-=1
-    def delete_last(self):
-        if(self._size==0):
-            return None
-        newest=self._head
-        prev=None
-        while(newest._link is not None):
-            prev=newest
-            newest=newest._link
-        if prev is None:
-            self._head==None
-        else:
-            prev._link=None
-        self._size-=1
     def printer(self):
         newest=self._head
-        for i in range(self._size):
-            print(newest._item)
+        while(newest._link is not None):
+            print(newest._item, end=' ')
             newest=newest._link
-    def searcher(self, target):
-        newest=self._head
-        for i in range(self._size):
-            if(newest._item==target):
-                return i
-            newest=newest._link 
-        return None
+        print(newest._item)
+    def delete_first(self):
+        if(self.isNone()):
+            return None
+        else:
+            self._head=self._head._link
+            self._size-=1
+    def delete_last(self):
+        if(self.isNone()):
+            return None
+        else:
+            tail=self._head
+            prev=0
+            while(tail._link is not None):
+                prev=tail
+                tail=tail._link
+            prev._link=None
+            self._size-=1
+    def delete_item(self,item):
+        if(self.isNone()):
+            return None
+        else:
+            finder=self._head
+            prev=None
+            while(finder._item is not item):
+                prev=finder
+                finder=finder._link
+                if(finder._link==None):
+                    return None
+            prev._link=finder._link
+            self._size-=1
+    def searcher(self, item):
+        if(self.isNone()):
+            return None
+        else:
+            finder=self._head
+            for i in range(self._size):
+                if(finder._item==item):
+                    return i
+                finder=finder._link
 s=SingleList()
 s.appender_first('orange')
 s.appender_first('apple')
@@ -61,5 +79,6 @@ s.printer()
 print('---------------------------------------')
 s.delete_first()
 s.delete_last()
+s.delete_item('orang')
 s.printer()
 print(s.searcher('ball'))
