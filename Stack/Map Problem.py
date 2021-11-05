455b38b2a70544acb92e196a78d8ee27acd5f74f
PersShin <shinryan9@naver.com>
2021-11-05 18:27:12 +0900

    Making map fuction추가

--- Stack/Map Problem.py
+++ Stack/Map Problem.py
@@ -1,7 +1,18 @@
 #The Maping problem in Data Structure
 #Using Stack or Queue!
-import numpy as np
-from numpy.lib.nanfunctions import nanquantile
+#I added Making map fuction(mapMaking)
+def mapMaking(map):
+    length=len(map)
+    tmp=[[0] for i in range(length+2)]
+    print(tmp)
+    for i in range(length+1):
+        tmp[0].append(0)
+        tmp[length+1].append(0)
+    for i in range(0,length):
+        tmp[i+1].extend(map[i])
+        tmp[i+1].append(0)
+        print(tmp)
+    return tmp
 def move(rat,map):
     map[rat[-1][0]][rat[-1][1]]=0
     for i in range(rat[-1][0]-1,rat[-1][0]+2):
@@ -14,6 +25,9 @@ def move(rat,map):
         rat.pop(-1)
     return rat,map
 def find(map):
+    map=mapMaking(map)
+    #The map=mapMaking(map) is set to (m+2)*(n+2)-> The real map is m*n
+    print(map)
     rat=[[1,1]]
     while True:
         rat,map=move(rat,map)
@@ -21,7 +35,5 @@ def find(map):
         if((rat[-1][0]==len(map)-2) & (rat[-1][1]==len(map)-2)):
             break
     print(rat)
-#The map is set to (m+2)*(n+2)-> The real map is m*n
-#The reason is that
-map=[[0,0,0,0,0,0],[0,1,0,0,0,0],[0,1,1,0,1,0],[0,0,1,0,0,0],[0,1,0,1,1,0],[0,0,0,0,0,0]]
+map=[[1,0,0,0],[1,1,0,1],[0,1,0,0],[1,0,1,1]]
 find(map)
