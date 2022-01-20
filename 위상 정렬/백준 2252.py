96de0a3cc3d4f062a4f6fa763d084e1a95358347
DESKTOP-F8UMNF0\Lab <shinryan9@naver.com>
2022-01-20 12:05:54 +0900

    위상 정렬을 사용한 코드

--- 위상 정렬/백준 2252.py
+++ 위상 정렬/백준 2252.py
@@ -3,6 +3,8 @@ from collections import deque
 def alignment():
     result=[]
     queue=deque()
+
+    #그래프의 시작점을 찾는 코드
     for i in range(1,N+1):
         if visited[i]==0:
             queue.append(i)
@@ -12,6 +14,7 @@ def alignment():
         result.append(tmp)
         for i in graph[tmp]:
             visited[i]-=1
+            #위에 연결되는 노드가 더 이상 없을 떄 queue에 저장
             if visited[i]==0:
                 queue.append(i)
 
@@ -23,5 +26,6 @@ visited=[0 for _ in range(N+1)]
 for _ in range(M):
     tmp_x,tmp_y=map(int,sys.stdin.readline().split())
     graph[tmp_x].append(tmp_y)
+    #위에 연결되는 노드가 있으므로 visited 배열에 추가 한다.
     visited[tmp_y]+=1
 print(alignment())
